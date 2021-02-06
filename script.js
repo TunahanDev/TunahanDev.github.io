e401a2e3f0f8bd55ed4b3cc3e9333174ac06b04c
TunahanDev <tunahanyildiz1560@gmail.com>
2021-02-07 02:30:46 +0300

    s

--- script.js
+++
@@ -1,5 +0,0 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled',$(this).scrollTop()>150)
-    $('li').toggleClass('scrolled',$(this).scrollTop()>150)
-    $('span').toggleClass('scrolled',$(this).scrollTop()>150)
-});
