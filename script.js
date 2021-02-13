190ecf6b289033917c9248c90eb6edfcf9713609
TunahanDev <tunahanyildiz1560@gmail.com>
2021-02-14 01:44:49 +0300

    sa

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(window).scroll(function(){
-    $('nav').toggleClass('scrolled',$(this).scrollTop()>50)
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>30)
     
     
 });
 var typed = new Typed(".typing",{
+    strings :["Web Developer.","IT Student.","Photographer.","Bike Addict.",],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
+var typed = new Typed(".typing-2",{
     strings :["Web Developer.","IT Student.","Photographer.","Bike Addict.",],
     typeSpeed: 100,
     backSpeed: 50,
